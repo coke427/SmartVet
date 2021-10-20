54ef93de9e9f1efb4bd6b5360ec3eabf8298d381
Jorge Mantilla <cokemanti@hotmail.com>
2021-10-19 22:02:49 -0500

    first commit

---
+++ smarvet_api/src/main/java/com/smarvet_api/SmarvetApiApplication.java
@@ -0,0 +1,13 @@
+package com.smarvet_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmarvetApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SmarvetApiApplication.class, args);
+    }
+
+}
---
+++ smarvet_api/src/test/java/com/smarvet_api/SmarvetApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.smarvet_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmarvetApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
