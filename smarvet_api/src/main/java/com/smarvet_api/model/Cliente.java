bc76a7c7a5828439651f21f219c3e5f9ba0e464f
Jorge Mantilla <cokemanti@hotmail.com>
2021-10-22 21:25:01 -0500

    (Backend)Implementación de @Size y el Hibernate para Regittro de client

--- smarvet_api/src/main/java/com/smarvet_api/model/Cliente.java
+++ smarvet_api/src/main/java/com/smarvet_api/model/Cliente.java
@@ -1,8 +1,8 @@
 package com.smarvet_api.model;
 
 import com.sun.istack.NotNull;
-
 import javax.persistence.*;
+import javax.validation.constraints.Size;
 
 @Entity
 @Table(name="clientes")
@@ -13,14 +13,17 @@ public class Cliente {
     private Integer idcliente;
 
     @NotNull
+    @Size(min= 2, message = "Nombre debe tener como minimo 2 caracteres")
     @Column (name="nombre_cliente", nullable = false, length = 30)
     private String nombreCliente;
 
     @NotNull
+    @Size(min= 3, message = "Apellido debe tener como minimo 3 caracteres")
     @Column (name="apellido_cliente", nullable = false, length = 30)
     private String apellidoCliente;
 
     @NotNull
+    @Size(min= 8, message = "Dni debe tener 8 caracteres")
     @Column (name="dni_cliente", nullable = false, length = 8)
     private String dniCliente;
 
@@ -30,7 +33,7 @@ public class Cliente {
     @Column (name="celular_cliente", nullable = true, length = 9)
     private String celularCliente;
 
-    @Column (name="direccion_cliente", nullable = true, length = 100)
+    @Column (name="direccion_cliente", nullable = true, length = 200)
     private String direccionCliente;
 
     @Column (name="email_cliente", nullable = true, length = 100)
