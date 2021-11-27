cf8420e9a8929d9cd3b144152f15014f6deff55d
Jorge Mantilla <cokemanti@hotmail.com>
2021-11-26 19:10:03 -0500

    Validaciones

--- smarvet_api/src/main/java/com/smarvet_api/validators/ClienteValidator.java
+++ smarvet_api/src/main/java/com/smarvet_api/validators/ClienteValidator.java
@@ -1,10 +1,38 @@
-package validators;
+package com.smarvet_api.validators;
 
+import com.smarvet_api.model.Cliente;
+import com.smarvet_api.exception.IncorrectResourceRequestException;
 
 public class ClienteValidator {
 
     public static void validate(Cliente cliente) {
 
+        if(cliente.getNombreCliente() == null || cliente.getNombreCliente().trim().isEmpty()) {
+            throw new IncorrectResourceRequestException("El Nombre es requerido");
+        }
 
+        if(cliente.getNombreCliente().length() < 3) {
+            throw new IncorrectResourceRequestException("El Nombre debe tener como minimo 3 letras");
+        }
+
+        if(cliente.getApellidoCliente() == null || cliente.getApellidoCliente().trim().isEmpty()) {
+            throw new IncorrectResourceRequestException("El Apellido es requerido");
+        }
+
+        if(cliente.getApellidoCliente().length() < 3) {
+            throw new IncorrectResourceRequestException("El Apellido debe tener como minimo 3 letras");
+        }
+
+        if(cliente.getDniCliente().length() == 8) {
+            throw new IncorrectResourceRequestException("El dni debe tener 8 caracteres");
+        }
+
+        if(cliente.getTelefonoCliente().length() == 6) {
+            throw new IncorrectResourceRequestException("El numero de telefono debe tener 6 caracteres");
+        }
+
+        if(cliente.getCelularCliente().length() == 9) {
+            throw new IncorrectResourceRequestException("El numero de celular debe tener 9 caracteres");
+        }
     }
 }
--- smarvet_api/src/main/java/com/smarvet_api/validators/VeterinarioValidator.java
+++ smarvet_api/src/main/java/com/smarvet_api/validators/VeterinarioValidator.java
@@ -1,4 +1,38 @@
-package validators;
+package com.smarvet_api.validators;
+
+import com.smarvet_api.model.Veterinario;
+import com.smarvet_api.exception.IncorrectResourceRequestException;
 
 public class VeterinarioValidator {
+
+    public static void validate(Veterinario veterinario) {
+
+        if(veterinario.getNombreVeterinario() == null || veterinario.getNombreVeterinario().trim().isEmpty()) {
+            throw new IncorrectResourceRequestException("El Nombre es requerido");
+        }
+
+        if(veterinario.getNombreVeterinario().length() < 3) {
+            throw new IncorrectResourceRequestException("El Nombre debe tener como minimo 3 letras");
+        }
+
+        if(veterinario.getApellidoVeterinario() == null || veterinario.getApellidoVeterinario().trim().isEmpty()) {
+            throw new IncorrectResourceRequestException("El Apellido es requerido");
+        }
+
+        if(veterinario.getApellidoVeterinario().length() < 3) {
+            throw new IncorrectResourceRequestException("El Apellido debe tener como minimo 3 letras");
+        }
+
+        if(veterinario.getDniVeterinario().length() == 8) {
+            throw new IncorrectResourceRequestException("El dni debe tener 8 caracteres");
+        }
+
+        if(veterinario.getTelefonoVeterinario().length() == 6) {
+            throw new IncorrectResourceRequestException("El numero de telefono debe tener 6 caracteres");
+        }
+
+        if(veterinario.getCelularVeterinario().length() == 9) {
+            throw new IncorrectResourceRequestException("El numero de celular debe tener 9 caracteres");
+        }
+    }
 }
